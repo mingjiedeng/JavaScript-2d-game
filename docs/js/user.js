b67e3c91fecc882daa85c509410f8058e01dfed0
Mingjie Deng <mingjie.dmj@gmail.com>
2018-08-27 08:17:26 -0700

    Open 9 levels for reviewing

--- docs/js/user.js
+++ docs/js/user.js
@@ -4,8 +4,8 @@
 class User {
   constructor(userName) {
     this.userName = userName;
-    this.levelRecords = [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    // this.levelRecords = [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0];
+    // this.levelRecords = [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.levelRecords = [3, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0];
   }
 }
 
